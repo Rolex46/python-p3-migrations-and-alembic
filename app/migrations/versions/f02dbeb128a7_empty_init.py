3f9930e2573b9892fc346ab52747f8c9786ca168
Rolex Omondi <rolexomondi46@gmail.com>
2023-06-02 11:19:30 +0300

    Completed assignemnt

---
+++ app/migrations/versions/f02dbeb128a7_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: f02dbeb128a7
+Revises: 
+Create Date: 2023-06-02 10:36:31.716199
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f02dbeb128a7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
